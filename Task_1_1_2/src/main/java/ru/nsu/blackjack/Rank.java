9d727c4bbf3d68e54f5f685fba7cdc89c649e0a4
nexlly <k.tyuzhin@g.nsu.ru>
2026-09-19 18:26:53 +0700

    create Rank file

---
+++ Task_1_1_2/src/main/java/ru/nsu/blackjack/Rank.java
@@ -0,0 +1,37 @@
+package ru.nsu.blackjack;
+
+public enum Rank {
+    TWO("Двойка", 2),
+    THREE("Тройка", 3),
+    FOUR("Четверка", 4),
+    FIVE("Пятерка", 5),
+    SIX("Шестерка", 6),
+    SEVEN("Семерка", 7),
+    EIGHT("Восьмерка", 8),
+    NINE("Девятка", 9),
+    TEN("Десятка", 10),
+    JACK("Валет", 10),
+    QUEEN("Дама", 10),
+    KING("Король", 10),
+    ACE("Туз", 11);
+
+    private final String title;
+    private final int value;
+
+    Rank(String title, int value) {
+        this.title = title;
+        this.value = value;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isAce() {
+        return this == ACE;
+    }
+}
